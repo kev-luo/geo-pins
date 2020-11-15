7af94f7c238e66617a2019d67fddebd245bb76b3
Kevin Luo <kvn.luo@gmail.com>
2020-11-15 18:59:36 -0500

    redirect to home page if user is logged in

--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -1,8 +1,13 @@
-import React from 'react'
-import Login from '../components/Auth/Login'
+import React, { useContext } from 'react';
+import Login from '../components/Auth/Login';
+import { Redirect } from 'react-router-dom';
+
+import UserContext from '../utils/UserContext';
 
 export default function Splash() {
+  const { state } = useContext(UserContext);
+
   return (
-    <Login />
+    state.isAuth ? <Redirect to="/" /> : <Login />
   )
 }

a6f5241d55a76203d6aa2231c0e25b277dac0bda
Kevin Luo <kvn.luo@gmail.com>
2020-11-19 09:52:51 -0500

    no content component styling

--- client/src/components/pin/NoContent.js
+++ client/src/components/pin/NoContent.js
@@ -1,24 +1,36 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import ExploreIcon from "@material-ui/icons/Explore";
 
-const useStyles = makeStyles(themes => ({
+const useStyles = makeStyles((themes) => ({
   root: {
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'column',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "center",
   },
   icon: {
     margin: themes.spacing.unit,
-    fontSize: '80px',
-  }
-}))
+    fontSize: "80px",
+  },
+}));
 
 export default function NoContent() {
   const classes = useStyles();
   return (
-    <div>
-      No Content
+    <div className={classes.root}>
+      <ExploreIcon className={classes.icon} />
+      <Typography
+        noWrap
+        component="h2"
+        variant="h6"
+        align="center"
+        color="textPrimary"
+        gutterBottom
+      >
+        Click on the map to add a pin
+      </Typography>
     </div>
-  )
+  );
 }

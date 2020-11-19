ec7962ee460e00a7fb42942b5d2c73d432ffecb4
Kevin Luo <kvn.luo@gmail.com>
2020-11-19 10:12:32 -0500

    add state for creating pin

--- client/src/components/pin/CreatePin.js
+++ client/src/components/pin/CreatePin.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, Typography, TextField } from '@material-ui/core';
 import LandscapeIcon from '@material-ui/icons/Landscape';
@@ -6,23 +6,62 @@ import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 import ClearIcon from '@material-ui/icons/Clear';
 import SaveIcon from '@material-ui/icons/Save';
 
-const useStyles = makeStyles(themes => ({
-  form: {
+const useStyles = makeStyles(theme => ({
+  form: { // this centers the form vertically
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
-    paddingBottom: themes.spacing.unit,
+    paddingBottom: theme.spacing.unit,
   },
-  contentField: {
-    marginLeft: themes.spacing.unit,
-    marginRight: themes.spacing.unit,
+  contentField: { // this makes the text field wider
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
     width: '95%',
+  },
+  input: { // this hides the input field where you add a photo
+    display: 'none',
+  },
+  alignCenter: { // makes the title a little less spread out
+    display: 'flex',
+    alignItems: 'center',
+  },
+  iconLarge: {
+    fontSize: 40,
+    marginRight: theme.spacing.unit
+  },
+  leftIcon: {
+    fontSize: 20,
+    marginRight: theme.spacing.unit,
+  },
+  rightIcon: {
+    fontSize: 20,
+    marginRight: theme.spacing.unit
+  },
+  button: { // spreads out the form a bit
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit,
+    marginLeft: 0,
   }
 }))
 
 export default function CreatePin() {
   const classes = useStyles();
+  const [text, setText] = useState({
+    title: '',
+    content: '',
+  })
+  const [image, setImage] = useState('');
+
+  const handleTextChange = e => {
+    const { name, value } = e.target;
+    setText({
+      ...text,
+      [name]: value
+    })
+  }
+
   return (
     <form className={classes.form}>
       <Typography
@@ -38,12 +77,14 @@ export default function CreatePin() {
           name='title'
           label='Title'
           placeholder='Insert pin title'
+          onChange={handleTextChange}
         />
         <input 
           accept='image/*'
           id='image'
           type='file'
           className={classes.input}
+          onChange={e => setImage(e.target.files[0])}
         />
         <label htmlFor='image'>
           <Button
@@ -62,6 +103,7 @@ export default function CreatePin() {
           margin='normal'
           fullWidth
           variant='outlined'
+          onChange={handleTextChange}
         />
       </div>
       <div>

6d771d996f1c8ff051c35f759f7e011bd11b5eea
Kevin Luo <kvn.luo@gmail.com>
2020-11-21 13:44:29 -0500

    comments text styling

--- client/src/components/comment/Comments.js
+++ client/src/components/comment/Comments.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Typography,
+} from "@material-ui/core";
 
-export default function Comments() {
+export default function Comments({ comments }) {
+  const classes = useStyles();
   return (
-    <div>
-      Comments
-    </div>
-  )
+    <List className={classes.root}>
+      {comments.map((comment, index) => {
+        return (
+          <ListItem key={index}>
+            <ListItemAvatar>
+              <Avatar src={comment.author.picture} alt={comment.author.name} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={comment.text}
+              secondary={
+                <Typography
+                  className={classes.inline}
+                  component="span"
+                  color="textPrimary"
+                >
+                  {comment.author.name}
+                </Typography>
+              }
+            />
+          </ListItem>
+        );
+      })}
+    </List>
+  );
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: "inline",
+  },
+}));

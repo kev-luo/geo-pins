615e46258a8f5e1415768d3021e666ab32ce1816
Kevin Luo <kvn.luo@gmail.com>
2020-11-21 14:55:40 -0500

    setup apolloclient and websocketlink to listen to published data

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws'
 
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000/graphql',
+  options: {
+    reconnect: true
+  }
+})
+
+const client = new ApolloClient({
+  link: wsLink,
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
 ,
   document.getElementById('root')
 );
